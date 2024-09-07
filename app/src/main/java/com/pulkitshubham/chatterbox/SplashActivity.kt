c0f34afcd22b1f3dcf9b90bc44fb5db32878b0eb
Pulkit Shubham <pulkitshubham11@gmail.com>
2024-09-07 20:11:00 +0530

    Splash Screen Added

---
+++ app/src/main/java/com/pulkitshubham/chatterbox/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.pulkitshubham.chatterbox
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.os.Handler
+import android.os.Looper
+import android.content.Intent
+import android.view.WindowManager
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        },3000)
+    }
+}
